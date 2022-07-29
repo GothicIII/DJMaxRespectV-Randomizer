1d2fc0510afa2b594d1426604448cb70376a6cb9
GothicIII <tom-matern@web.de>
2022-07-29 10:25:01 +0200

    Fixed input and song order ranges

--- DJMax_Detection.h
+++ DJMax_Detection.h
@@ -15,25 +15,81 @@
 ;// This library is not able to fetch data consistantly so manual intervention is neccessary to produce all results. 
 ;//
 
+refresh:=1 
+
+; Chart data definition in memory
+class Generate_Chart_Data_old
+{
+	__New(name, songgroup, fourkdata, fivekdata, sixkdata, eightkdata)
+	{
+	global refresh, debugstring
+	static alphaarr := fillarr(26,0)
+	if refresh=1
+	{
+		alphaarr := fillarr(26,0)
+		refresh:=0
+	}
+	esg:=EvaluateSongGroup(songgroup)
+	this.name 	:= name
+	if esg=0 or esg=2
+		this.order:=-1
+	else 
+		if ord(strupper(this.name))-64 < 1 
+			this.order:=0
+		else
+			this.order	:= ++alphaarr[ord(strupper(this.name))-64]
+	;debugfunc(name, this.order, songgroup)
+	this.fourk	:= {}
+	this.fivek	:= {}
+	this.sixk	:= {}
+	this.eightk	:= {}
+	
+	this.fourk.nm	:= fourkdata[1]
+	this.fourk.hd	:= fourkdata[2]
+	this.fourk.mx	:= fourkdata[3]
+	this.fourk.sc	:= fourkdata[4]
+	
+	this.fivek.nm	:= fivekdata[1]
+	this.fivek.hd	:= fivekdata[2]
+	this.fivek.mx	:= fivekdata[3]
+	this.fivek.sc	:= fivekdata[4]
+	
+	this.sixk.nm	:= sixkdata[1]
+	this.sixk.hd	:= sixkdata[2]
+	this.sixk.mx	:= sixkdata[3]
+	this.sixk.sc	:= sixkdata[4]
+	
+	this.eightk.nm	:= eightkdata[1]
+	this.eightk.hd	:= eightkdata[2]
+	this.eightk.mx	:= eightkdata[3]
+	this.eightk.sc	:= eightkdata[4]
+	if esg>1
+		this.sg := "CO"
+	else 
+		this.sg := songgroup
+	}
+} 
+
 Class song_order_numbers
 {
 	__New()
 	{
 	;//[currentline (always 1, but if detection fails, you can start from a later song by providing the songcount), beginning line in namesdb of that songpack, number of songs]
-	this.re 	:= [1,1,80]
-	this.pone 	:= [1,81,56]
-	this.ptwo	:= [1,137,53]
-	this.pthree	:= [1,190,22]
-	this.tr		:= [1,212,20]
-	this.cl		:= [1,232,24]
-	this.bs		:= [1,256,21]
-	this.vone	:= [1,277,20]
-	this.vtwo	:= [1,297,21]
-	this.es		:= [1,318,8]
-	this.tone	:= [1,326,21]
-	this.ttwo	:= [1,347,23]
-	this.tthree	:= [1,370,29]
-	this.co		:= [1,399,70]
+	this.re 	:= [1,1,82]
+	this.pone 	:= [1,83,56]
+	this.ptwo	:= [1,139,53]
+	this.pthree	:= [1,192,22]
+	this.tr		:= [1,214,20]
+	this.cl		:= [1,234,24]
+	this.bs		:= [1,258,21]
+	this.vone	:= [1,279,20]
+	this.vtwo	:= [1,299,21]
+	this.es		:= [1,320,8]
+	this.tone	:= [1,328,21]
+	this.ttwo	:= [1,349,23]
+	this.tthree	:= [1,372,29]
+	this.tq		:= [1,401,19]
+	this.co		:= [1,420,70]
 	}
 }
 
@@ -71,13 +127,16 @@ Return
 ;// creates a difficulty array and detects how many difficulty charts exist for that song.
 CreateDiffArray(buttoncolor, TTwo)
 {
-	buttoncolorTtwo:=["0xFFBA00",	"0xFD6306", "0xFE00D8", "0xFF0042"]
+	buttoncolorTtwo:=["0xFFBA00", "0xFD6306", "0xFE00D8", "0xFF0042"]
 	xbuttondiffpos:=[133,293,452,613]
 	y:=667
 	diff_arr:=[]
+	
+	
 	for curdiff in xbuttondiffpos
 	{
 		WinActivate("ahk_exe DJMax Respect V.exe")
+		;Msgbox(PixelGetColor(curdiff,y))
 		if TTwo=1
 		{
 			buttoncolor:=buttoncolorTtwo[A_Index]
@@ -86,7 +145,9 @@ CreateDiffArray(buttoncolor, TTwo)
 		{
 			diff_arr.push(GetSongDiff())
 			WinActivate("ahk_exe DJMax Respect V.exe")
-			Send "{Right}"
+			Send "{Right Down}"
+			Sleep 25
+			Send "{Right Up}"
 			Sleep 100
 			continue
 		}
@@ -135,6 +196,8 @@ FetchSongname(SongGroup)
 			suffix:="ttwo"
 		Case "T3":
 			suffix:="tthree"
+		Case "TQ":
+			suffix:="tq"
 		Case "CO":
 			suffix:="co"
 	}
@@ -146,7 +209,11 @@ FetchSongname(SongGroup)
 		Send "{RShift up}" 
 		}
 		else
-			Send "{Down}"
+		{
+			Send "{Down Down}"
+			Sleep 25
+			Send "{Down Up}"
+		}
 		Sleep 300
 	loop parse SongNamesDb, "`n"
 		if A_Index=songorder.%suffix%[2]+songorder.%suffix%[1]-1
@@ -162,13 +229,6 @@ GetSongData(){
 	lastsonggroup:=songgroup:=""
 	while songgroup!="CO" or A_Index<=85 ;Number of collaboration songs
 	{
-		; This block is only useful to skip Muse Dash DLC since I do not own it.
-		if songgroup="CO" and A_Index>=50 and A_Index<=64 
-		{
-			Send "{Down}"
-			Sleep 100
-			continue
-		}
 		songgroup:=GetSongGroup()
 		if lastsonggroup!=songgroup
 		{
@@ -205,22 +265,28 @@ GetSongData(){
 				Ttwo:=1
 			Case "T3":
 				buttoncolor := "0x1257EE"
+			Case "TQ":
+				buttoncolor := "0x999999"
 			Case "CO":
 				buttoncolor := "0xFFFFFF"
 		}
 		fourkdata:=CreateDiffArray(buttoncolor, Ttwo)
-		Send "5"
-		sleep 50
+		Send "{5 down}"
+		Sleep 25
+		Send "{5 up}"
 		fivekdata:=CreateDiffArray(buttoncolor, Ttwo)
-		Send "6"
-		sleep 50
+		Send "{6 down}"
+		Sleep 25
+		Send "{6 up}"
 		sixkdata:=CreateDiffArray(buttoncolor, Ttwo)
-		Send "8"
-		sleep 50
+		Send "{8 down}"
+		Sleep 25
+		Send "{8 up}"
 		eightkdata:=CreateDiffArray(buttoncolor, Ttwo)
-		Send "4"
-		sleep 50
-		AppendSongData(Generate_Chart_Data(FetchSongname(songgroup), songgroup, fourkdata, fivekdata, sixkdata, eightkdata))
+		Send "{4 down}"
+		Sleep 25
+		Send "{4 up}"
+		AppendSongData(Generate_Chart_Data_old(FetchSongname(songgroup), songgroup, fourkdata, fivekdata, sixkdata, eightkdata))
 	}
 }
 
@@ -277,6 +343,8 @@ WinActivate("ahk_exe DJMax Respect V.exe")
 		Return "T2"
 	Case "0x514CF7":
 		Return "T3"
+	Case "0xBBBBBB":
+		Return "TQ"	
 	Case "0x4C4C4C":
 		Return "CO"
 	}
